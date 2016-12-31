92eb12757aae04eb8f6b33d768c34f5cc80897ca
SCIS Security <dchow@xtecsystems.com>
2016-12-30 18:06:33 -0600

    eDiscovery_fileShares_r2.ps1
    A simple interactive PowerShell script that performs basic eDiscovery searches across UNC path file shares or logical drives (local or mapped). The script performs essentially a 'string' search with direct support for regular expressions. It will iterate and recurse a base directory to perform a grep across all files and write out to a list of files that are responsive. It then copies the files to the desired destination keeping metadata attributes including MAC times. It will exclude ACL/Security permission migration so that a 3rd party can view without having the SID or wasting time 'taking ownership'. MD5 hashes are calculated for each file based on the SOURCE file and then dumped into a file. DISCLAIMER: No warranty. This software is under GPLv2 and community based. If you like it, please keep the original author information within.

---
+++ eDiscovery_fileShares_r2.ps1
@@ -0,0 +1,119 @@
+﻿#dchow[AT]xtecsystems.com
+<#eDiscovery String Search and Copy files to a specific destination that Match
+ For PS v3 and higher. Recommended on PS v5 however 20161227 dc
+ *Notice* IOPS performance runs best when not ran from a cmd.exe wrapper
+ vR1 (1.1) - Changelog 20161228 dc
+    Added file date creation range search
+    Fixed 'InputStream' odd string from EOF before processing
+    Changed Hashing to MD5 for performance purposes
+    Added check to see if PS v5 is installed
+
+ vR2 (1.2) - Changelog 20161229 dc
+    Fixed memory exhaustion problem that not loading recurse dir in memory. (May impact indexing performance though)
+    Changed copy mechanism to robocopy.exe
+
+#>
+
+#Version Check
+If ( $PSVersionTable.PSVersion.Major -ge 3)
+{
+    #Collect user input
+    [string]$stringInput = Read-Host -prompt "Enter string or regex to search for e.g. foo"
+    Write-Host "You entered $stringInput" -foregroundcolor green -backgroundcolor black
+    [string]$startDateInput = Read-Host -prompt "Enter START search date range (CREATED DATE) MM/DD/YYYY"
+    Write-Host "You entered $startDateInput" -foregroundcolor green -backgroundcolor black
+    [string]$endDateInput = Read-Host -prompt "Enter END search date range (CREATED DATE) MM/DD/YYYY"
+    Write-Host "You entered $endDateInput" -foregroundcolor green -backgroundcolor black
+    [string]$pathInput = Read-Host -Prompt "Enter the base path to start search from e.g. H:\*.* or \\foo\bar"
+    Write-Host "You entered $pathInput" -foregroundcolor green -backgroundcolor black
+    [string]$logsOut = Read-host -Prompt "Enter the output path to export log results to e.g. c:\foo\log.txt"
+    Write-Host "You entered $logsOut" -foregroundcolor green -backgroundcolor black
+    [string]$copyDest = Read-Host -prompt "Enter the destination path for found files e.g. \\foo\bar"
+    Write-Host "You entered $copyDest" -foregroundcolor green -backgroundcolor black
+    [string]$hashFile = Read-Host -prompt "Enter the output path to export hash results to e.g. c:\foo\hash.txt"
+    Write-Host "You entered $hashFile" -foregroundcolor green -backgroundcolor black
+
+    #Enumerate files with conditions
+    Try
+    {
+        Write-Host "Searching for $stringInput in files starting in $pathInput" -foregroundcolor green -backgroundcolor black
+        Write-Host "Constraints Create Date Range: $startDateInput through $endDateInput" -foregroundcolor green -backgroundcolor black
+        <#
+        Get-ChildItem -Path "$pathInput" -Recurse |Where-Object { $_.CreationTime -gt "$startDateInput" -and $_.CreationTime -le "$endDateInput" } `
+        | Select-String -Pattern $stringInput | ForEach { $_.Path } | Get-Unique | Tee-Object -FilePath $logsOut -Append
+        #>
+
+        #ForEach-Object stream to memory rather that front load. I/O performance may suffer
+        #However the trade up is optimization so you do not exhaust the default 1 GB allocated to each PS console instance
+        Get-ChildItem -Path "$pathInput" -Recurse | ForEach-Object {
+            If ( ($_.CreationTime -gt "$startDateInput" -and $_.CreationTime -le "$endDateInput"))
+            {
+                $_ | Select-String -Pattern $stringInput | ForEach { $_.Path } | Get-Unique | Tee-Object -FilePath $logsOut -Append
+            }
+        }
+    }
+    Catch
+    {
+        Write-Host "An error condition has occured. Please check your inputs." -foregroundcolor red -backgroundcolor yellow
+        echo $_.Exception | Format-List -Force
+    }
+
+
+    #Copy files enumerated from list to destination
+    Try
+    {
+        Write-Host "Copying responsive files to $copyDest" -foregroundcolor green -backgroundcolor black
+        
+        ForEach ($x in (Get-Content $logsOut))
+        {
+  
+            <#
+            Will Change MAC "date created" times on the copied folder. Does not save attributes
+            Robocopy.exe only requires the source directory listed with the filename separated to work
+            #>
+            #Copy-Item -Path $x -Destination $copyDest -ErrorAction Continue
+            
+            $resultsBasePath = Split-Path -Path $x
+            $fileNamePath = $x | Split-Path -Leaf
+
+            robocopy.exe $resultsBasePath $copyDest $fileNamePath '/COPY:DAT'
+
+        } 
+    }
+    Catch
+    {
+        Write-Host "An error condition has occured. Please check your inputs." -foregroundcolor red -backgroundcolor yellow
+        echo $_.Exception | Format-List -Force
+    }
+
+    #Calculate MD5 hashes 
+    Try
+    {
+        Write-Host "Calculating Hashes to $hashFile" -foregroundcolor green -backgroundcolor black
+        ForEach ($y in (Get-Content $logsOut))
+        {
+            echo (Get-FileHash $y -Algorithm MD5 | Format-List)  >> $hashFile
+        }
+    }
+    Catch
+    {
+        Write-Host "An error condition has occured. Please check your inputs." -foregroundcolor red -backgroundcolor yellow
+        echo $_.Exception | Format-List -Force
+    }
+
+    Write-Host "Copies and hashing completed." -foregroundcolor green -backgroundcolor black
+    Write-Host "Your files are located in the following locations:"
+    Write-Host $logsOut
+    Write-Host $copyDest
+    Write-Host $hashFile
+
+#Exit PS to clear out memory
+exit
+}
+Else
+    {
+    Write-Host "You need PS version 3 or higher." -foregroundcolor red -backgroundcolor yellow
+    Write-Host "Download WMF v5 here: https://www.microsoft.com/en-us/download/details.aspx?id=50395 "
+    Write-Host "Exiting..."
+    exit
+    }
